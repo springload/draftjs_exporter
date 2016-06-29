137654f7720f8d99c58a15532b03fd521b1411e8
Thibaud Colas <thibaudcolas@gmail.com>
2016-06-30 00:40:52 +0300

    Finish implementation of style state

--- draft_exporter/style_state.py
+++ draft_exporter/style_state.py
@@ -1,9 +1,47 @@
+import re
+
+# TODO Extract to utils
+# https://gist.github.com/yahyaKacem/8170675
+_first_cap_re = re.compile(r'(.)([A-Z][a-z]+)')
+_all_cap_re = re.compile('([a-z0-9])([A-Z])')
+
+
+def camelToDash(camelCasedStr):
+    sub2 = _first_cap_re.sub(r'\1-\2', camelCasedStr)
+    dashed_case_str = _all_cap_re.sub(r'\1-\2', sub2).lower()
+    return dashed_case_str.replace('--', '-')
 
 
 class StyleState():
+    """
+    Handles the creation of inline styles on elements.
+    Receives inline_style commands, and generates the element's `style`
+    attribute from those.
+    """
     def __init__(self, style_map):
         self.styles = []
         self.style_map = style_map
 
     def apply(self, command):
-        pass
+        if (command.name == 'start_inline_style'):
+            self.styles.append(command.data)
+        elif (command.name == 'stop_inline_style'):
+            self.styles.remove(command.data)
+
+    def is_unstyled(self):
+        return not self.styles
+
+    def element_attributes(self):
+        return {} if len(self.styles) == 0 else {
+            'style': self.get_style_value()
+        }
+
+    def get_style_value(self):
+        rules = []
+
+        for style in self.styles:
+            css_style = self.style_map.get(style, {})
+            for prop in css_style.keys():
+                rules.append('{0}: {1};'.format(camelToDash(prop), css_style[prop]))
+
+        return ''.join(rules)
--- tests/test_style_state.py
+++ tests/test_style_state.py
@@ -1,9 +1,12 @@
 import unittest
+
+from draft_exporter.command import Command
 from draft_exporter.style_state import StyleState
 
 style_map = {
     'ITALIC': {'fontStyle': 'italic'},
-    'BOLD': {'fontStyle': 'bold'}
+    'BOLD': {'fontStyle': 'bold'},
+    'HIGHLIGHT': {'fontStyle': 'bold', 'textDecoration': 'underline'},
 }
 
 
@@ -14,14 +17,37 @@ class TestStyleState(unittest.TestCase):
     def test_init(self):
         self.assertIsInstance(self.style_state, StyleState)
 
-    @unittest.skip('TODO')
     def test_apply_start_inline_style(self):
         self.assertEquals(self.style_state.styles, [])
-        self.style_state.apply({'name': 'start_inline_style', 'data': {}})
+        self.style_state.apply(Command('start_inline_style', 0, 'ITALIC'))
+        self.assertEquals(self.style_state.styles, ['ITALIC'])
+
+    def test_apply_stop_inline_style(self):
+        self.style_state.apply(Command('start_inline_style', 0, 'ITALIC'))
+        self.style_state.apply(Command('stop_inline_style', 0, 'ITALIC'))
         self.assertEquals(self.style_state.styles, [])
 
-    @unittest.skip('TODO')
-    def test_apply_stop_inline_style(self):
-        self.assertEquals(self.style_state.styles, [])
-        self.style_state.apply({'name': 'stop_inline_style', 'data': {}})
-        self.assertEquals(self.style_state.styles, [])
+    def test_is_unstyled_default(self):
+        self.assertEquals(self.style_state.is_unstyled(), True)
+
+    def test_is_unstyled_styled(self):
+        self.style_state.apply(Command('start_inline_style', 0, 'ITALIC'))
+        self.assertEquals(self.style_state.is_unstyled(), False)
+
+    def test_get_style_value_empty(self):
+        self.assertEquals(self.style_state.get_style_value(), '')
+
+    def test_get_style_value_single(self):
+        self.style_state.apply(Command('start_inline_style', 0, 'ITALIC'))
+        self.assertEquals(self.style_state.get_style_value(), 'font-style: italic;')
+
+    def test_get_style_value_multiple(self):
+        self.style_state.apply(Command('start_inline_style', 0, 'HIGHLIGHT'))
+        self.assertEquals(self.style_state.get_style_value(), 'font-style: bold;text-decoration: underline;')
+
+    def test_element_attributes_empty(self):
+        self.assertEquals(self.style_state.element_attributes(), {})
+
+    def test_element_attributes_single(self):
+        self.style_state.apply(Command('start_inline_style', 0, 'ITALIC'))
+        self.assertEquals(self.style_state.element_attributes(), {'style': 'font-style: italic;'})
