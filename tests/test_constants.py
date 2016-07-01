3e7f194f19a8413be7f0747302901cf8d5a5dea1
Thibaud Colas <thibaudcolas@gmail.com>
2016-07-01 12:20:28 +0300

    Add missing tests

--- tests/test_constants.py
+++ tests/test_constants.py
@@ -8,9 +8,12 @@ from draftjs_exporter.constants import BLOCK_TYPES, ENTITY_TYPES, INLINE_STYLES
 class TestConstants(unittest.TestCase):
     def test_block_types(self):
         self.assertIsInstance(BLOCK_TYPES, object)
+        self.assertEqual(BLOCK_TYPES.UNSTYLED, 'unstyled')
 
     def test_entity_types(self):
         self.assertIsInstance(ENTITY_TYPES, object)
+        self.assertEqual(ENTITY_TYPES.LINK, 'LINK')
 
     def test_inline_styles(self):
         self.assertIsInstance(INLINE_STYLES, object)
+        self.assertEqual(INLINE_STYLES.BOLD, 'BOLD')
