927fec4ad05403a5badaa9b2c97aa112218a548b
Thibaud Colas <thibaudcolas@gmail.com>
2016-11-16 21:05:40 +0200

    Remove tests and logic for invalid attributes for Link

--- draftjs_exporter/entities.py
+++ draftjs_exporter/entities.py
@@ -27,23 +27,12 @@ class Image:
 
 
 class Link:
-    attributes = ['url', 'rel', 'target', 'title']
-
-    @staticmethod
-    def is_valid_attribute(key):
-        # TODO How much do we need to whitelist / blacklist attributes?
-        valid_data_attr = key.startswith('data-') and len(key) > 5 and key.islower()
-        return key in Link.attributes or valid_data_attr
-
     def render(self, props):
         data = props.get('data', {})
-        # TODO attributes will be in an unpredictable sort order. Use elt.set(attr, val) instead?
-        # Use from collections import OrderedDict?
         attributes = {}
         for key in data:
-            if data[key] and Link.is_valid_attribute(key):
-                attr = key if key != 'url' else 'href'
-                attributes[attr] = data[key]
+            attr = key if key != 'url' else 'href'
+            attributes[attr] = data[key]
 
         return DOM.create_element('a', attributes)
 
