8805d4ad5665c56f8835a100b24408a42b72df60
Loic Teixeira <loic@springload.co.nz>
2016-11-16 16:39:27 +1300

    Add tests for Enum

--- tests/test_constants.py
+++ tests/test_constants.py
@@ -2,7 +2,21 @@ from __future__ import absolute_import, unicode_literals
 
 import unittest
 
-from draftjs_exporter.constants import BLOCK_TYPES, ENTITY_TYPES, INLINE_STYLES
+from draftjs_exporter.constants import Enum, BLOCK_TYPES, ENTITY_TYPES, INLINE_STYLES
+
+
+class EnumConstants(unittest.TestCase):
+    def test_enum_returns_the_key_if_valid(self):
+        foo_value = 'foo'
+        e = Enum(foo_value)
+
+        self.assertEqual(e.foo, foo_value)
+
+    def test_enum_raises_an_error_for_invalid_keys(self):
+        e = Enum('foo', 'bar')
+
+        with self.assertRaises(AttributeError):
+            e.invalid_key
 
 
 class TestConstants(unittest.TestCase):
