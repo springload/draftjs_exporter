2cb45fc8e9ba92391d11c2f370368d60046fa8b1
Thibaud Colas <thibaudcolas@gmail.com>
2016-12-10 09:57:03 +1300

    Release v0.5.2

--- draftjs_exporter/__init__.py
+++ draftjs_exporter/__init__.py
@@ -1,6 +1,6 @@
 
 __title__ = 'draftjs_exporter'
-__version__ = '0.5.1'
+__version__ = '0.5.2'
 __author__ = 'Springload'
 __license__ = 'MIT'
-__copyright__ = 'Copyright 2016 Springload'
+__copyright__ = 'Copyright 2017 Springload'
