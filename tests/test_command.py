d3b8a38210e19f832d69880919d234d070360108
Thibaud Colas <thibaudcolas@gmail.com>
2017-02-16 01:50:45 +0200

    Remove unused code

--- tests/test_command.py
+++ tests/test_command.py
@@ -24,48 +24,6 @@ class TestCommand(unittest.TestCase):
     def test_key(self):
         self.assertEqual(Command.key(self.command), 5)
 
-    def test_grouped_by_index(self):
-        grouped = Command.grouped_by_index([
-            Command('start_text', 0),
-            Command('stop_text', 19),
-            Command('start_inline_style', 0, 'ITALIC'),
-            Command('stop_inline_style', 4, 'ITALIC'),
-            Command('start_inline_style', 9, 'BOLD'),
-            Command('stop_inline_style', 12, 'BOLD'),
-            Command('start_entity', 5, 0),
-            Command('stop_entity', 14, 0),
-            Command('start_entity', 0, 1),
-            Command('stop_entity', 4, 1),
-        ])
-        flattened = [(index, list(group)) for index, group in grouped]
-
-        self.assertEqual(str(flattened), str([
-            (0, [
-                Command('start_text', 0),
-                Command('start_inline_style', 0, 'ITALIC'),
-                Command('start_entity', 0, 1),
-            ]),
-            (4, [
-                Command('stop_inline_style', 4, 'ITALIC'),
-                Command('stop_entity', 4, 1),
-            ]),
-            (5, [
-                Command('start_entity', 5, 0),
-            ]),
-            (9, [
-                Command('start_inline_style', 9, 'BOLD'),
-            ]),
-            (12, [
-                Command('stop_inline_style', 12, 'BOLD'),
-            ]),
-            (14, [
-                Command('stop_entity', 14, 0),
-            ]),
-            (19, [
-                Command('stop_text', 19),
-            ]),
-        ]))
-
     def test_start_stop(self):
         self.assertEqual(str(Command.start_stop('abracadabra', 0, 5, 'shazam')), str([
             Command('start_abracadabra', 0, 'shazam'),
