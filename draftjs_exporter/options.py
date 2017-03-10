be6ac06f229f293310194f2e191374b7c9ae4a48
Thibaud Colas <thibaudcolas@gmail.com>
2017-03-10 16:25:24 +0200

    Rename BlockException to ConfigException

---
+++ draftjs_exporter/options.py
@@ -0,0 +1,49 @@
+from __future__ import absolute_import, unicode_literals
+
+from draftjs_exporter.error import ExporterException
+
+
+class ConfigException(ExporterException):
+    pass
+
+
+class Options:
+    """
+    Facilitates querying configuration from the block_map.
+    """
+    def __init__(self, element, props=None, wrapper=None, wrapper_props=None):
+        self.element = element
+        self.props = props
+        self.wrapper = wrapper
+        self.wrapper_props = wrapper_props
+
+    def __str__(self):
+        return '<Options {0} {1} {2} {3}>'.format(self.element, self.props, self.wrapper, self.wrapper_props)
+
+    def __repr__(self):
+        return str(self)
+
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
+
+    def __ne__(self, other):
+        return not self == other
+
+    @staticmethod
+    def for_block(block_map, type_):
+        if type_ not in block_map:
+            raise ConfigException('Block "%s" does not exist in block_map' % type_)
+
+        block = block_map.get(type_)
+
+        if isinstance(block, list):
+            raise ConfigException('Block "%s" uses unsupported list-style config' % type_)
+        elif isinstance(block, dict):
+            if 'element' not in block:
+                raise ConfigException('Block "%s" does not define an element' % type_)
+
+            opts = Options(**block)
+        else:
+            opts = Options(block)
+
+        return opts
