fda251e4d472696f9f94d77fce222698c315796e
Thibaud Colas <thibaudcolas@gmail.com>
2017-03-10 18:28:04 +0200

    Simplify styles rendering to rely completely on create_element

--- draftjs_exporter/style_state.py
+++ draftjs_exporter/style_state.py
@@ -24,21 +24,13 @@ class StyleState:
         return not self.styles
 
     def render_styles(self, text_node):
-        if self.is_empty():
-            node = text_node
-        else:
+        node = text_node
+        if not self.is_empty():
             options = [Options.for_style(self.style_map, s) for s in self.styles]
-            options.sort(key=lambda o: o.element)
-
-            node = DOM.create_element(options[0].element, options[0].props)
-            child = node
+            options.sort(key=lambda o: o.element, reverse=True)
 
             # Nest the tags.
-            for opt in options[1:]:
-                new_child = DOM.create_element(opt.element, opt.props)
-                DOM.append_child(child, new_child)
-                child = new_child
-
-            DOM.append_child(child, text_node)
+            for opt in options:
+                node = DOM.create_element(opt.element, opt.props, node)
 
         return node
