82bb04c93771df7e9b98315bda5cc7234ff85909
Thibaud Colas <thibaudcolas@gmail.com>
2017-03-18 01:10:44 +0800

    Add tests for new wrapper_state features

--- tests/test_wrapper_state.py
+++ tests/test_wrapper_state.py
@@ -6,6 +6,22 @@ from draftjs_exporter.dom import DOM
 from draftjs_exporter.wrapper_state import WrapperState
 
 
+def Blockquote(props):
+    block_data = props['block']['data']
+
+    return DOM.create_element('blockquote', {
+        'cite': block_data.get('cite')
+    }, props['children'])
+
+
+def ListItem(props):
+    depth = props['block']['depth']
+
+    return DOM.create_element('li', {
+        'class': 'list-item--depth-{0}'.format(depth)
+    }, props['children'])
+
+
 class TestWrapperState(unittest.TestCase):
     def setUp(self):
         self.wrapper_state = WrapperState({
@@ -13,6 +29,8 @@ class TestWrapperState(unittest.TestCase):
             'unstyled': 'div',
             'atomic': lambda props: props['children'],
             'ignore': None,
+            'blockquote': Blockquote,
+            'unordered-liste-item': ListItem,
         })
 
     def test_init(self):
@@ -58,6 +76,19 @@ class TestWrapperState(unittest.TestCase):
             'entityRanges': []
         }, DOM.create_element('img', {'src': '/example.png'}))), '<img src="/example.png"/>')
 
+    def test_element_for_component(self):
+        self.assertEqual(DOM.render(self.wrapper_state.element_for({
+            'key': '5s7g9',
+            'text': 'Paragraph',
+            'type': 'blockquote',
+            'depth': 0,
+            'data': {
+                'cite': 'http://example.com/',
+            },
+            'inlineStyleRanges': [],
+            'entityRanges': []
+        }, 'Test')), '<blockquote cite="http://example.com/">Test</blockquote>')
+
     def test_to_string_empty(self):
         self.assertEqual(self.wrapper_state.to_string(), '')
 
@@ -87,3 +118,23 @@ class TestWrapperState(unittest.TestCase):
         }, '')
 
         self.assertEqual(str(self.wrapper_state), '<WrapperState: <h1></h1>>')
+
+
+class TestBlockquote(unittest.TestCase):
+    def test_render(self):
+        self.assertEqual(DOM.render(DOM.create_element(Blockquote, {
+            'block': {
+                'data': {
+                    'cite': 'http://example.com/',
+                },
+            },
+        }, 'Test')), '<blockquote cite="http://example.com/">Test</blockquote>')
+
+
+class TestListItem(unittest.TestCase):
+    def test_render(self):
+        self.assertEqual(DOM.render(DOM.create_element(ListItem, {
+            'block': {
+                'depth': 5,
+            },
+        }, 'Test')), '<li class="list-item--depth-5">Test</li>')
