704f313a2652206eb885879acef3df85cf721309
Thibaud Colas <thibaudcolas@gmail.com>
2017-03-25 21:52:03 +1300

    Reduce over-reliance on DOM methods

--- tests/test_dom.py
+++ tests/test_dom.py
@@ -17,29 +17,28 @@ class TestDOM(unittest.TestCase):
         self.assertEqual(DOM.render(DOM.create_element('p', {'style': 'border-color: red;text-decoration: underline;'}, 'Test test')), '<p style="border-color: red;text-decoration: underline;">Test test</p>')
 
     def test_create_element_empty(self):
-        self.assertEqual(DOM.get_tag_name(DOM.create_element()), 'fragment')
+        self.assertEqual(DOM.render_debug(DOM.create_element()), '<fragment></fragment>')
 
     def test_create_element_nested(self):
-        self.assertEqual(DOM.render(DOM.create_element('a', {}, DOM.create_element('span', {'className': 'file-info icon-text'}, DOM.create_element('span', {'className': 'icon-text__text'}, 'Test test'), DOM.create_element('svg', {'className': 'icon'}, DOM.create_element('use', {'xlink:href': '#icon-test'}))))), '<a><span class="file-info icon-text"><span class="icon-text__text">Test test</span><svg class="icon"><use xlink:href="#icon-test"></use></svg></span></a>')
+        self.assertEqual(DOM.render_debug(DOM.create_element('a', {}, DOM.create_element('span', {'className': 'file-info icon-text'}, DOM.create_element('span', {'className': 'icon-text__text'}, 'Test test'), DOM.create_element('svg', {'className': 'icon'}, DOM.create_element('use', {'xlink:href': '#icon-test'}))))), '<a><span class="file-info icon-text"><span class="icon-text__text">Test test</span><svg class="icon"><use xlink:href="#icon-test"></use></svg></span></a>')
 
     def test_create_element_none(self):
-        self.assertEqual(DOM.render(DOM.create_element('a', {}, None, DOM.create_element('span', {}, 'Test test'))), '<a><span>Test test</span></a>')
+        self.assertEqual(DOM.render_debug(DOM.create_element('a', {}, None, DOM.create_element('span', {}, 'Test test'))), '<a><span>Test test</span></a>')
 
     def test_create_element_entity(self):
-        self.assertEqual(DOM.render(DOM.create_element(Icon, {'name': 'rocket'})), '<svg class="icon"><use xlink:href="#icon-rocket"></use></svg>')
+        self.assertEqual(DOM.render_debug(DOM.create_element(Icon, {'name': 'rocket'})), '<svg class="icon"><use xlink:href="#icon-rocket"></use></svg>')
 
     def test_create_element_entity_configured(self):
-        self.assertEqual(DOM.render(DOM.create_element(Icon(icon_class='i'), {'name': 'rocket'})), '<svg class="i"><use xlink:href="#icon-rocket"></use></svg>')
+        self.assertEqual(DOM.render_debug(DOM.create_element(Icon(icon_class='i'), {'name': 'rocket'})), '<svg class="i"><use xlink:href="#icon-rocket"></use></svg>')
 
     def test_create_document_fragment(self):
-        self.assertEqual(DOM.get_tag_name(DOM.create_document_fragment()), 'fragment')
+        self.assertEqual(DOM.render_debug(DOM.create_document_fragment()), '<fragment></fragment>')
 
     def test_create_text_node(self):
-        self.assertEqual(DOM.get_tag_name(DOM.create_text_node('Test text')), 'textnode')
-        self.assertEqual(DOM.get_text_content(DOM.create_text_node('Test text')), 'Test text')
+        self.assertEqual(DOM.render_debug(DOM.create_text_node('Test text')), '<textnode>Test text</textnode>')
 
     def test_parse_html(self):
-        self.assertEqual(DOM.render(DOM.parse_html('<p><span>Test text</span></p>')), '<p><span>Test text</span></p>')
+        self.assertEqual(DOM.render_debug(DOM.parse_html('<p><span>Test text</span></p>')), '<p><span>Test text</span></p>')
 
     def test_camel_to_dash(self):
         self.assertEqual(DOM.camel_to_dash('testCamelToDash'), 'test-camel-to-dash')
@@ -52,18 +51,7 @@ class TestDOM(unittest.TestCase):
     def test_append_child(self):
         parent = DOM.create_element('p')
         DOM.append_child(parent, DOM.create_element('span', {}, 'Test text'))
-        self.assertEqual(DOM.render(parent), '<p><span>Test text</span></p>')
-
-    def test_set_attribute(self):
-        elt = DOM.create_element('a')
-        DOM.set_attribute(elt, 'href', 'http://example.com')
-        self.assertEqual(DOM.render(elt), '<a href="http://example.com"></a>')
-
-    def test_get_tag_name(self):
-        self.assertEqual(DOM.get_tag_name(DOM.create_element('p', {}, 'Test test')), 'p')
-
-    def test_get_class_list(self):
-        self.assertEqual(DOM.get_class_list(DOM.create_element('p', {'className': 'intro test'}, 'Test test')), ['intro', 'test'])
+        self.assertEqual(DOM.render_debug(parent), '<p><span>Test text</span></p>')
 
     def test_get_text_content(self):
         self.assertEqual(DOM.get_text_content(DOM.create_element('p', {}, 'Test test')), 'Test test')
