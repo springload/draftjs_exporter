91ef593de91e0a48a5dc1747fd3962632a4be9f9
Thibaud Colas <thibaudcolas@gmail.com>
2017-04-09 10:38:03 +0800

    Refine defaults for block map

--- tests/test_defaults.py
+++ tests/test_defaults.py
@@ -2,7 +2,8 @@ from __future__ import absolute_import, unicode_literals
 
 import unittest
 
-from draftjs_exporter.defaults import BLOCK_MAP, STYLE_MAP
+from draftjs_exporter.defaults import BLOCK_MAP, STYLE_MAP, CodeBlock, render_children
+from draftjs_exporter.dom import DOM
 
 
 class TestDefaults(unittest.TestCase):
@@ -11,3 +12,11 @@ class TestDefaults(unittest.TestCase):
 
     def test_default_style_map(self):
         self.assertIsInstance(STYLE_MAP, object)
+
+    def test_render_children(self):
+        self.assertEqual(render_children({'children': 'test'}), 'test')
+
+
+class TestCodeBlock(unittest.TestCase):
+    def test_render(self):
+        self.assertEqual(DOM.render_debug(CodeBlock({'children': 'test'})), '<pre><code>test</code></pre>')
