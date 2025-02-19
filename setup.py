32c5748b43c2546042ce33482eb0f376c0ce316a
Thibaud Colas <thibaudcolas@gmail.com>
2025-02-19 09:12:33 +0000

    Upgrade to lxml 5

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     package_data={"draftjs_exporter": ["py.typed"]},
     extras_require={
         # Keep this in sync with the dependencies in setup.py, requirements.txt, tox.ini.
-        "lxml": ["lxml>=4.2.0,<5"],
+        "lxml": ["lxml>=4.2.0,<6"],
         "html5lib": ["beautifulsoup4>=4.4.1,<5", "html5lib>=0.999,<2"],
     },
     zip_safe=False,
